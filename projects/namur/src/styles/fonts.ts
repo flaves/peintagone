b5b7ba71ca0482ddb752cf1a24c654b82688bc1b
Romain Lambert <romain.lambert912@gmail.com>
2021-01-17 13:36:09 +0100

    :bug: (package.json) Testing linting pre-commit hook

--- projects/namur/src/styles/fonts.ts
+++ projects/namur/src/styles/fonts.ts
@@ -3,12 +3,10 @@ import { css } from '@emotion/react';
 // Biennale
 import BiennaleHeavy from '../fonts/biennale-heavy.otf';
 
-const BiennaleFont = css`
+export const BiennaleFont = css`
   @font-face {
     font-family: Biennale;
     font-weight: 900;
     src: url(${BiennaleHeavy});
   }
 `;
-
-export default BiennaleFont;
