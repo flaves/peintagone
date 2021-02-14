d1d80e567ca87b77ea3c6c69816edb9aa7b0da81
Sebastien Crepin <sebastien@flav.es>
2021-02-14 15:31:46 +0100

    :bug: (contact/form) Fix form

--- projects/namur/src/components/atoms/Form/Form/Form.tsx
+++ projects/namur/src/components/atoms/Form/Form/Form.tsx
@@ -1,4 +1,5 @@
 import React, { FormHTMLAttributes } from 'react';
+import { useFormContext } from 'react-hook-form';
 
 interface Props extends FormHTMLAttributes<HTMLFormElement> {
   children: React.ReactNode;
@@ -6,15 +7,22 @@ interface Props extends FormHTMLAttributes<HTMLFormElement> {
 }
 
 const Form = ({ children, name, ...rest }: Props): JSX.Element => {
+  const { register } = useFormContext();
+
   return (
     <form
       name={name}
-      netlify-honeypot="bot-field"
       data-netlify="true"
+      data-netlify-honeypot="bot-field"
       {...rest}
     >
       <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value={name} />
+      <input
+        type="hidden"
+        name="form-name"
+        value={name}
+        ref={register({ required: true })}
+      />
       {children}
     </form>
   );
