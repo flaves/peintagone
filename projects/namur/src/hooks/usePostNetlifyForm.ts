817e706edee0ba0278ae8b30f1cc803ac73dbe77
Sebastien Crepin <sebastien@flav.es>
2021-02-15 20:10:04 +0100

    :bug: (contact/form) Fix error message form

--- projects/namur/src/hooks/usePostNetlifyForm.ts
+++ projects/namur/src/hooks/usePostNetlifyForm.ts
@@ -11,22 +11,11 @@ const usePostNetlifyForm = (url: string, formName: string): OutProps => {
   const { showSuccess, showError } = useNotifications();
   const [loading, setLoading] = useState<boolean>(false);
 
-  const checkStatus = (res: Response): Promise<JSON> | null => {
-    if (res.ok) {
-      // res.status >= 200 && res.status < 300
-      return res.json();
-    }
-    showError(`Une erreur est survenue, code: ${res.status}`);
-    throw new Error(res.statusText);
-  };
-
   const executeFetch = async (data: any) => {
     setLoading(true);
 
-    console.log('executing fetch ...');
-
     try {
-      const response = await fetch(url, {
+      await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: qs.stringify({
@@ -35,26 +24,11 @@ const usePostNetlifyForm = (url: string, formName: string): OutProps => {
         }),
       });
 
-      const test = await checkStatus(response);
-
-      console.log('check status result => ', test);
-
-      setLoading(false);
       showSuccess('Votre formulaire a bien été envoyé');
-
-      return {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: 'Form sent',
-      };
     } catch (e) {
-      showError('Désolé une erreur est survenue, veuillez réassayer');
+      showError('Désolé une erreur est survenue, veuillez réessayer');
+    } finally {
       setLoading(false);
-
-      return {
-        statusCode: 500,
-        body: e.message,
-      };
     }
   };
 
