c51649665be2604cc76fcc4d2b8a76ca487212c7
Sebastien Crepin <sebastien@flav.es>
2021-03-23 10:29:17 +0100

    :chart_with_upwards_trend: (config) Add GA code tracking

--- projects/namur/gatsby-config.js
+++ projects/namur/gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `TO_REPLACE`,
+        trackingId: `G-JFBFJGHXL5`,
       },
     },
     {
