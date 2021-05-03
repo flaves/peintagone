0a8a187695f9d1cbafbf343ecaf5f461cef3be31
Sebastien Crepin <sebastien@flav.es>
2021-05-03 11:10:23 +0200

    :sparkles: (tracking) Add Google Ads code

--- projects/namur/gatsby-ssr.js
+++ projects/namur/gatsby-ssr.js
@@ -8,3 +8,19 @@ export const wrapPageElement = ({ element, props }) => {
 };
 
 export const wrapRootElement = wrapWithProvider;
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="google-tag-manager"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=AW-458385190"
+    />,
+    <script
+      key="gtag"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-458385190');`,
+      }}
+    />,
+  ]);
+};
